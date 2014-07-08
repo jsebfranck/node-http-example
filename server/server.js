76df936cddec2fe00620ba17ddb2f2d4f47d1b8c
jsebfranck <jsebfranck@gmail.com>
2014-07-08 23:56:18 +0200

    Added basic express server

--- server/server.js
+++ server/server.js
@@ -1,2 +1,15 @@
 'use strict';
 
+var express = require('express'),
+  app = express();
+
+app.get('/count', function(req, res) {
+  console.log('count method has been called');
+  res.send({
+    count: 1200
+  });
+});
+
+app.listen(3000, function() {
+  console.log('App started');
+});
