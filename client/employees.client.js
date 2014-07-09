ee76a1690e1b0265ff940208cd6d9db8b29bf2c3
jsebfranck <jsebfranck@gmail.com>
2014-07-09 22:48:39 +0200

    Test with real http call

--- client/employees.client.js
+++ client/employees.client.js
@@ -1,11 +1,24 @@
 'use strict';
 
-var Q = require('Q');
+var Q = require('Q'),
+  http = require('http');
 
 exports.countEmployees = function() {
   var deferred = Q.defer();
 
-  deferred.resolve(0);
+  var options = {
+    hostname: 'localhost',
+	port: 3000,
+	path: '/employees/count'
+  };
+
+  http.get(options, function(res) {
+    res.setEncoding('utf8');
+    res.on('data', function(chunk) {
+      var employeesCount = JSON.parse(chunk).count;
+      deferred.resolve(employeesCount);
+    });
+  });
 
   return deferred.promise;
 };
--- tests/employees.client.test.js
+++ tests/employees.client.test.js
@@ -4,10 +4,10 @@ var should = require('should'),
   client = require('../client/employees.client');
 
 describe('employees count service', function() {
-  it('should return 0', function(done) {
+  it('should return 1200', function(done) {
     client.countEmployees().then(function(count) {
       try {
-        count.should.equal(0);
+        count.should.equal(1200);
         done();
       } catch (assertionError) {
         done(assertionError);
